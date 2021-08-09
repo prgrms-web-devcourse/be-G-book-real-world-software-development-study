9641797336583296ffe7b144c5b0858e2826a683
cse0518 <g787@naver.com>
2021-08-09 17:53:31 +0900

    소스코드 update

---
+++ Chapter_02/src/main/java/com/realdevelopment/chapter2/MainApplication.java
@@ -0,0 +1,16 @@
+package com.realdevelopment.chapter2;
+
+public class MainApplication {
+
+    public static void main(String[] args) throws Exception {
+
+        final BankStatementAnalyzer bankStatementAnalyzer
+                = new BankStatementAnalyzer();
+
+        final BankStatementParser bankStatementParser
+                = new BankStatementCSVParser();
+
+        bankStatementAnalyzer.analyze("bank-data-simple.csv", bankStatementParser);
+
+    }
+}
