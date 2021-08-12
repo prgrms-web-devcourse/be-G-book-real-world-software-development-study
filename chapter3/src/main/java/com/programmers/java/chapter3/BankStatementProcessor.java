38f9adcba437e8e9fe1ad6b3f560138515eafabe
ilyongHwang <1yongs_@naver.com>
2021-08-12 15:22:05 +0900

    ADD: 3-6. 명시적 API vs 암묵적 API

--- chapter3/src/main/java/com/programmers/java/chapter3/BankStatementProcessor.java
+++ chapter3/src/main/java/com/programmers/java/chapter3/BankStatementProcessor.java
@@ -3,17 +3,28 @@ package com.programmers.java.chapter3;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BankStatementProcessor {
+
     private final List<BankTransaction> bankTransactions;
 
     public BankStatementProcessor(final List<BankTransaction> bankTransactions) {
         this.bankTransactions = bankTransactions;
     }
 
+    public double summarizeTransactions(final BankTransactionSummarizer bankTransactionSummarizer) {
+        double result = 0;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            result = bankTransactionSummarizer.summarize(result, bankTransaction);
+        }
+
+        return result;
+    }
+
     public double calculateTotalAmount() {
         double total = 0;
-        for (final BankTransaction bankTransaction: bankTransactions) {
+        for (final BankTransaction bankTransaction : bankTransactions) {
             total += bankTransaction.getAmount();
         }
         return total;
@@ -21,7 +32,7 @@ public class BankStatementProcessor {
 
     public double calculateTotalInMonth(final Month month) {
         double total = 0;
-        for (final BankTransaction bankTransaction: bankTransactions) {
+        for (final BankTransaction bankTransaction : bankTransactions) {
             if (bankTransaction.getDate().getMonth() == month)
                 total += bankTransaction.getAmount();
         }
@@ -30,7 +41,7 @@ public class BankStatementProcessor {
 
     public double calculateTotalForCategory(final String category) {
         double total = 0;
-        for (final BankTransaction bankTransaction: bankTransactions) {
+        for (final BankTransaction bankTransaction : bankTransactions) {
             if (bankTransaction.getDescription().equals(category))
                 total += bankTransaction.getAmount();
         }
@@ -41,12 +52,29 @@ public class BankStatementProcessor {
     public List<BankTransaction> findTransactions(final BankTransactionFilter bankTransactionFilter) {
         final List<BankTransaction> result = new ArrayList<>();
 
-        for (final BankTransaction bankTransaction: bankTransactions) {
+        for (final BankTransaction bankTransaction : bankTransactions) {
             if (bankTransactionFilter.test(bankTransaction)) {
                 result.add(bankTransaction);
             }
         }
 
         return result;
+    }
+
+    public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
+        /** case 1.
+         * 특정 BankTransactionFilter 구현(class BankTransactionIsInFebruaryAndExpensive)으로 findTransactions () 호출
+         *
+         * final List<BankTransaction> transactions = findTransactions(new BankTransactionIsInFebruaryAndExpensive());
+         */
+        /** case 2.
+         *  람다 표현식으로 BankTransactionFilter 구현하기
+         *
+         *  final List<BankTransaction> transactions = findTransactions(bankTransaction ->
+         *      bankTransaction.getDate().getMont() == Month.FEBRUARY
+         *      && bankTransaction.getAmount() >= 1_000);
+         *      );
+         */
+        return findTransactions(bankTransaction -> bankTransaction.getAmount() >= amount);
     }
 }
