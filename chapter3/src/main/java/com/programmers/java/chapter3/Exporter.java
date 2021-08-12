8b2147ad1644e4d11382e0cf2d794b05e410b553
ilyongHwang <1yongs_@naver.com>
2021-08-12 15:39:29 +0900

    ADD: 3-7. 다양한 형식으로 내보내기

---
+++ chapter3/src/main/java/com/programmers/java/chapter3/Exporter.java
@@ -0,0 +1,7 @@
+package com.programmers.java.chapter3;
+
+import com.programmers.java.chapter3.domain.SummaryStatistics;
+
+public interface Exporter {
+    String export(SummaryStatistics summaryStatistics);
+}
