a7cbaf70912133bca92a02fce4257e75e90e4db2
ilyongHwang <1yongs_@naver.com>
2021-08-12 17:46:04 +0900

    ADD: 3.8~3.9 예외 처리, 빌드 도구

--- chapter3/src/main/java/com/programmers/java/chapter3/BankTransaction.java
+++ chapter3/src/main/java/com/programmers/java/chapter3/BankTransaction.java
@@ -1,6 +1,9 @@
 package com.programmers.java.chapter3;
 
+import com.programmers.java.chapter3.domain.Notification;
+
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.Objects;
 
 public class BankTransaction {
@@ -47,6 +50,34 @@ public class BankTransaction {
 
     @Override
     public int hashCode() {
-        return Objects.hash(date ,amount, description);
+        return Objects.hash(date, amount, description);
+    }
+
+    public Notification validate() {
+
+        final Notification notification = new Notification();
+
+        if (this.description.length() > 100) {
+            notification.addError("The description is too long");
+        }
+
+        final LocalDate parsedDate;
+        try {
+            parsedDate = LocalDate.parse(this.date.toString());
+            if (this.date.isAfter(LocalDate.now())) {
+                notification.addError("date cannot be in the future");
+            }
+        } catch (DateTimeParseException e) {
+            notification.addError("Invalid format for date");
+        }
+
+        final double amount;
+        try {
+            amount = Double.parseDouble(Double.toString(this.amount));
+        } catch (NumberFormatException e) {
+            notification.addError("Invalid format for amount");
+        }
+
+        return notification;
     }
 }
